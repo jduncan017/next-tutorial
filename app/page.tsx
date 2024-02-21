ead916e2f7d5bf70ce425f528edf23eb4b8b09a5
jduncan017 <emailjoshduncan@gmail.com>
2024-02-20 17:06:48 -0700

    update page info

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,11 @@ export default function Page() {
           <p
             className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome!</strong> This is an educational project for{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Learning Next.js
             </a>
-            , brought to you by Vercel.
+            , built by Josh Duncan.
           </p>
           <Link
             href="/login"
